98a02dc634c3965b8e34ec7317cb45a980ddda48
yellowsea <yunhai0644@163.com>
2022-05-14 15:56:08 +0800

    feat:  Swipe img

--- src/mock/data/swiperList.ts
+++ src/mock/data/swiperList.ts
@@ -1,18 +1,18 @@
 export default [
   {
     link: '',
-    imgSrc: 'https://i0.hdslb.com/bfs/archive/bf2aa1f18ccae9ecae4cb666417f75da951ee2f4.jpg@480w_300h.webp'
+    imgSrc: 'https://i0.hdslb.com/bfs/banner/122cc470fa4f2de4b334b691adcd44a212531bc7.png@976w_550h_1c.webp'
   },
   {
     link: '',
-    imgSrc: 'https://i0.hdslb.com/bfs/archive/8860d7a57a9b1992c112ee56a35444cfc445108b.jpg@480w_300h.webp'
+    imgSrc: 'https://i0.hdslb.com/bfs/archive/be5f3d335e733d7b5d9d130058432045ab34e22f.jpg@672w_378h_1c.webp'
   },
   {
     link: '',
-    imgSrc: 'https://i0.hdslb.com/bfs/archive/82232bca677f06d69734159653cf29db82ff5d9c.png@480w_300h.webp'
+    imgSrc: 'https://i0.hdslb.com/bfs/banner/fe9b0982b2fe6fff7bdd2f687fc7427c0387f594.jpg@976w_550h_1c.webp'
   },
   {
     link: '',
-    imgSrc: 'https://i0.hdslb.com/bfs/archive/28d93d03968baa475a2c3982640736a66b4405c3.jpg@480w_300h.webp'
+    imgSrc: 'https://i0.hdslb.com/bfs/feed-admin/2626d89270ca7cf41bed4038d928f13e4df76a8e.png@976w_550h_1c.webp'
   }
 ]
--- src/views/Home/components/home-swipe.vue
+++ src/views/Home/components/home-swipe.vue
@@ -1,28 +1,32 @@
 <template >
   <!-- 轮播图 -->
   <van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-    <van-swipe-item>
-      <img src="">
+    <van-swipe-item v-for="item in swipeList" :key="item.imgSrc">
+      <img :src="item.imgSrc" />
     </van-swipe-item>
   </van-swipe>
 </template>
 <script lang="ts" setup>
 import axios from 'axios'
-import {ref} from 'vue'
+import { ref } from 'vue'
 
+interface swipeItem {
+  imgSrc: string;
+  link: string;
+}
+
+const swipeList = ref<swipeItem[]>([])
 axios({
   url: '/swiperList',
   method: 'get'
 }).then(res => {
-  console.log(res)
+  swipeList.value = res.data.result
 })
 </script>
 <style lang="less">
-.my-swipe .van-swipe-item {
-  color: #fff;
-  font-size: 20px;
-  line-height: 150px;
-  text-align: center;
-  background-color: #39a9ed;
+.my-swipe {
+  img { 
+    width: 100%;
+  }
 }
 </style>
